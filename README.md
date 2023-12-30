02b9d2ffac1c8b68f3b2bf73c5d30bd07e579c87
kvartiil <138615442+kvartiil@users.noreply.github.com>
2023-12-30 20:01:00 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# matrix84
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/kvartiil/matrix84)
---
+++ src/app/matrixRateDict/matrixRateDict.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from "@angular/core";
+import ratios from "./valdRatios.json";
+import { environment, myNewConstant } from '../../environments/environments';
+
+@Component({
+  selector: "matrix-ratedict",
+  templateUrl: "./matrixRateDict.component.html",
+  styleUrls: ["./matrixRateDict.component.css"]
+})
+export class RateDictComponent implements OnInit { 
+
+  ngOnInit() {
+
+   
+
+}
+
+}
